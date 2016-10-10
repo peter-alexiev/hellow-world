bf90dfa1422716a5335d03ca9c7dbc9c4eb745f1
peter-alexiev <alexiev.peter@gmail.com>
2016-10-10 12:41:11 +0300

    Third output changed.

--- HelloWorld/src/main/Hello.java
+++ HelloWorld/src/main/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Second output.");
-		System.out.println("Third output.");
+		System.out.println("Third output......");
 	}
 
 }
